87f6a9672bc1b7e7c63855db7f16a77631b9b25e
Gilich Mateo <serviciosgilich@gmail.com>
2026-09-17 17:13:45 -0300

    Add files via upload

---
+++ InteractionRaycast.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+// Lanza un raycast desde la cámara hacia adelante para detectar objetos interactuables.
+// El Interactable Mask debe incluir SOLO la layer "Interactable" (no "Everything"),
+// así el rayo no choca contra el propio Player que está siempre en el camino.
+public class InteractionRaycast : MonoBehaviour
+{
+    [SerializeField] private float interactRange = 3f;
+    [SerializeField] private LayerMask interactableMask;
+
+    private Camera playerCamera;
+    private PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        playerCamera = Camera.main;
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Interact.performed += ctx => TryInteract();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void TryInteract()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, interactRange, interactableMask))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+            interactable?.Interact();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (playerCamera == null) return;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(playerCamera.transform.position, playerCamera.transform.position + playerCamera.transform.forward * interactRange);
+    }
+}
